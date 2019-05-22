c5b9045feebbc493fdff00a5866dfe3487a4d31d
Federico Bertola <fb@bendingspoons.com>
2019-05-22 18:20:12 +0200

    Setup with scikit-build

---
+++ setup.py
@@ -0,0 +1,11 @@
+from skbuild import setup
+
+setup(
+    name="bgfx-python",
+    version="0.0.1",
+    description="",
+    author="Federico Bertola",
+    license="MIT",
+    packages=['bgfx_python'],
+    cmake_source_dir="src"
+)
