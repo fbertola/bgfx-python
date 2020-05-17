bcf8e593bd2a1dbda04a90d51de0d427a117194f
Federico Bertola <federico.bertola88@gmail.com>
2020-05-17 23:35:45 +0200

    Fixed Linux image size

--- .travis/after_success.sh
+++ .travis/after_success.sh
@@ -9,8 +9,5 @@ for entry in wheelhouse/bgfx_python-*.whl ; do
 
   STATUS=$(curl --write-out %{http_code} --silent --output /dev/null -T $entry -ufbertola:$BINTRAY_API_KEY https://api.bintray.com/content/fbertola/bgfx-python-wheels/bgfx-python/${TRAVIS_BUILD_NUMBER}/$entry)
 
-  if [[ "$STATUS" -ne 200 ]] ; then
-    echo "Upload to Bintray: $STATUS"
-    exit 1
-  fi
+  echo "Upload to Bintray: $STATUS"
 done
