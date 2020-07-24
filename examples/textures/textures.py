07b2a100fb893d1b3b631b6e5425742d2b5510a4
Federico Bertola <federico.bertola88@gmail.com>
2020-07-24 17:08:18 +0200

    Fixed IB cleanup

--- examples/textures/textures.py
+++ examples/textures/textures.py
@@ -1,8 +1,10 @@
 import os
 from ctypes import Structure, c_float, c_uint32, sizeof
 from pathlib import Path
-from PIL import Image 
+
 import numpy as np
+from PIL import Image
+from loguru import logger
 
 from bgfx import (
     bgfx,
@@ -15,31 +17,17 @@ from bgfx import (
     as_void_ptr,
     ShaderType,
     load_shader,
-    BGFX_STATE_WRITE_R,
-    BGFX_STATE_WRITE_G,
-    BGFX_STATE_WRITE_B,
     BGFX_STATE_WRITE_A,
     BGFX_STATE_WRITE_Z,
     BGFX_STATE_DEPTH_TEST_LESS,
-    BGFX_STATE_CULL_CW,
     BGFX_STATE_MSAA,
-    BGFX_STATE_PT_POINTS,
-    BGFX_STATE_PT_LINES,
-    BGFX_STATE_PT_TRISTRIP,
-    BGFX_STATE_PT_LINESTRIP,
     BGFX_TEXTURE_RT,
-    BGFX_SAMPLER_MAG_ANISOTROPIC, 
-    BGFX_SAMPLER_U_MIRROR, 
-    BGFX_SAMPLER_V_MIRROR,
-    BGFX_SAMPLER_U_MIRROR, 
-    BGFX_SAMPLER_V_MIRROR,
-    BGFX_STATE_WRITE_RGB 
-    
+    BGFX_STATE_WRITE_RGB
+
 )
 from examples.example_window import ExampleWindow
 from examples.utils.imgui_utils import show_example_dialog
 from examples.utils.matrix_utils import look_at, proj, rotate_xy
-from loguru import logger
 
 logger.enable("bgfx")
 
@@ -50,59 +38,58 @@ class PosColorTexVertex(Structure):
         ("m_y", c_float),
         ("m_z", c_float),
         ("m_abgr", c_uint32),
-        ("m_tex_u",c_float),
-        ("m_tex_v",c_float)
+        ("m_tex_u", c_float),
+        ("m_tex_v", c_float)
     ]
 
 
 num_vertices = 24
 
 cube_vertices = (PosColorTexVertex * 24)(
-    PosColorTexVertex(-1.0,   1.0,  1.0,  0xFF000000,       0,     0    ),
-    PosColorTexVertex( 1.0,   1.0,  1.0,  0xFF000000,       1,     0    ),
-    PosColorTexVertex(-1.0,  -1.0,  1.0,  0xFF000000,       0,     1    ),
-    PosColorTexVertex( 1.0,  -1.0,  1.0,  0xFF000000,       1,     1    ),
-    PosColorTexVertex(-1.0,   1.0, -1.0,  0xFF000000,       0,     0    ),
-    PosColorTexVertex( 1.0,   1.0, -1.0,  0xFF000000,       1,     0    ),
-    PosColorTexVertex(-1.0,  -1.0, -1.0,  0xFF000000,       0,     1    ),
-    PosColorTexVertex( 1.0,  -1.0, -1.0,  0xFF000000,       1,     1    ),
-    PosColorTexVertex(-1.0,   1.0,  1.0,  0xFF000000,       0,     0    ),
-    PosColorTexVertex( 1.0,   1.0,  1.0,  0xFF000000,       1,     0    ),
-    PosColorTexVertex(-1.0,   1.0, -1.0,  0xFF000000,       0,     1    ),
-    PosColorTexVertex( 1.0,   1.0, -1.0,  0xFF000000,       1,     1    ),
-    PosColorTexVertex(-1.0,  -1.0,  1.0,  0xFF000000,       0,     0    ),
-    PosColorTexVertex( 1.0,  -1.0,  1.0,  0xFF000000,       1,     0    ),
-    PosColorTexVertex(-1.0,  -1.0, -1.0,  0xFF000000,       0,     1    ),
-    PosColorTexVertex( 1.0,  -1.0, -1.0,  0xFF000000,       1,     1    ),
-    PosColorTexVertex( 1.0,  -1.0,  1.0,  0xFF000000,       0,     0    ),
-    PosColorTexVertex( 1.0,   1.0,  1.0,  0xFF000000,       1,     0    ),
-    PosColorTexVertex( 1.0,  -1.0, -1.0,  0xFF000000,       0,     1    ),
-    PosColorTexVertex( 1.0,   1.0, -1.0,  0xFF000000,       1,     1    ),
-    PosColorTexVertex(-1.0,  -1.0,  1.0,  0xFF000000,       0,     0    ),
-    PosColorTexVertex(-1.0,   1.0,  1.0,  0xFF000000,       1,     0    ),
-    PosColorTexVertex(-1.0,  -1.0, -1.0,  0xFF000000,       0,     1    ),
-    PosColorTexVertex(-1.0,   1.0, -1.0,  0xFF000000,       1,     1    )
-    )
-	
-	
+    PosColorTexVertex(-1.0, 1.0, 1.0, 0xFF000000, 0, 0),
+    PosColorTexVertex(1.0, 1.0, 1.0, 0xFF000000, 1, 0),
+    PosColorTexVertex(-1.0, -1.0, 1.0, 0xFF000000, 0, 1),
+    PosColorTexVertex(1.0, -1.0, 1.0, 0xFF000000, 1, 1),
+    PosColorTexVertex(-1.0, 1.0, -1.0, 0xFF000000, 0, 0),
+    PosColorTexVertex(1.0, 1.0, -1.0, 0xFF000000, 1, 0),
+    PosColorTexVertex(-1.0, -1.0, -1.0, 0xFF000000, 0, 1),
+    PosColorTexVertex(1.0, -1.0, -1.0, 0xFF000000, 1, 1),
+    PosColorTexVertex(-1.0, 1.0, 1.0, 0xFF000000, 0, 0),
+    PosColorTexVertex(1.0, 1.0, 1.0, 0xFF000000, 1, 0),
+    PosColorTexVertex(-1.0, 1.0, -1.0, 0xFF000000, 0, 1),
+    PosColorTexVertex(1.0, 1.0, -1.0, 0xFF000000, 1, 1),
+    PosColorTexVertex(-1.0, -1.0, 1.0, 0xFF000000, 0, 0),
+    PosColorTexVertex(1.0, -1.0, 1.0, 0xFF000000, 1, 0),
+    PosColorTexVertex(-1.0, -1.0, -1.0, 0xFF000000, 0, 1),
+    PosColorTexVertex(1.0, -1.0, -1.0, 0xFF000000, 1, 1),
+    PosColorTexVertex(1.0, -1.0, 1.0, 0xFF000000, 0, 0),
+    PosColorTexVertex(1.0, 1.0, 1.0, 0xFF000000, 1, 0),
+    PosColorTexVertex(1.0, -1.0, -1.0, 0xFF000000, 0, 1),
+    PosColorTexVertex(1.0, 1.0, -1.0, 0xFF000000, 1, 1),
+    PosColorTexVertex(-1.0, -1.0, 1.0, 0xFF000000, 0, 0),
+    PosColorTexVertex(-1.0, 1.0, 1.0, 0xFF000000, 1, 0),
+    PosColorTexVertex(-1.0, -1.0, -1.0, 0xFF000000, 0, 1),
+    PosColorTexVertex(-1.0, 1.0, -1.0, 0xFF000000, 1, 1)
+)
 
 cube_indices = (np.array([
-     0,  2,  1,
-	 1,  2,  3,
-	 4,  5,  6,
-	 5,  7,  6,
+    0, 2, 1,
+    1, 2, 3,
+    4, 5, 6,
+    5, 7, 6,
 
-	 8, 10,  9,
-	 9, 10, 11,
-	12, 13, 14,
-	13, 15, 14,
+    8, 10, 9,
+    9, 10, 11,
+    12, 13, 14,
+    13, 15, 14,
 
-	16, 18, 17,
-	17, 18, 19,
-	20, 21, 22,
-	21, 23, 22],dtype=np.uint16))
+    16, 18, 17,
+    17, 18, 19,
+    20, 21, 22,
+    21, 23, 22], dtype=np.uint16))
 
 root_path = Path(__file__).parent.parent / "shaders"
+
 
 class Textures(ExampleWindow):
     def __init__(self, width, height, title):
@@ -138,28 +125,29 @@ class Textures(ExampleWindow):
 
         self.vertex_layout = bgfx.VertexLayout()
         self.vertex_layout.begin().add(
-              bgfx.Attrib.POSITION, 3, bgfx.AttribType.FLOAT
+            bgfx.Attrib.POSITION, 3, bgfx.AttribType.FLOAT
         ).add(bgfx.Attrib.COLOR0, 4, bgfx.AttribType.UINT8, True
-        ).add(bgfx.Attrib.TEXCOORD0, 2, bgfx.AttribType.FLOAT).end()
+              ).add(bgfx.Attrib.TEXCOORD0, 2, bgfx.AttribType.FLOAT).end()
 
         # Create static vertex buffer
         vb_memory = bgfx.copy(
             as_void_ptr(cube_vertices), sizeof(PosColorTexVertex) * num_vertices
         )
         self.vertex_buffer = bgfx.create_vertex_buffer(vb_memory, self.vertex_layout)
-        
+
         # Create index buffer
         ib_memory = bgfx.copy(as_void_ptr(cube_indices), cube_indices.nbytes)
         self.index_buffer = bgfx.create_index_buffer(ib_memory)
 
         # Create texture uniform
-        self.texture_uniform = bgfx.create_uniform("s_tex",  bgfx.UniformType.SAMPLER)
-       
+        self.texture_uniform = bgfx.create_uniform("s_tex", bgfx.UniformType.SAMPLER)
+
         # Load the image using PIL and make the texture
-        logo = Image.open(Path(__file__).parent /"python_logo.png")
+        logo = Image.open(Path(__file__).parent / "python_logo.png")
         logo_memory = bgfx.copy(as_void_ptr(logo.tobytes()), len(logo.tobytes()))
-        self.logo_texture = bgfx.create_texture2d(logo.width,logo.height,False,1,bgfx.TextureFormat.RGBA8, BGFX_TEXTURE_RT ,logo_memory)
-       
+        self.logo_texture = bgfx.create_texture2d(logo.width, logo.height, False, 1, bgfx.TextureFormat.RGBA8,
+                                                  BGFX_TEXTURE_RT, logo_memory)
+
         # Create program from shaders.
         self.main_program = bgfx.create_program(
             load_shader(
@@ -175,8 +163,7 @@ class Textures(ExampleWindow):
 
     def shutdown(self):
         ImGuiExtra.imgui_destroy()
-        for index_buffer in self.index_buffers:
-            bgfx.destroy(index_buffer)
+        bgfx.destroy(self.index_buffer)
         bgfx.destroy(self.vertex_buffer)
         bgfx.destroy(self.main_program)
         bgfx.shutdown()
@@ -209,26 +196,26 @@ class Textures(ExampleWindow):
                 mtx = rotate_xy(
                     self.elapsed_time + xx * 0.51, self.elapsed_time + yy * 0.27
                 )
-                mtx[3, 0] =  4+ xx * 3.5
-                mtx[3, 1] =  2 + yy * 3.5
-                mtx[3, 2] =  0
+                mtx[3, 0] = 4 + xx * 3.5
+                mtx[3, 1] = 2 + yy * 3.5
+                mtx[3, 2] = 0
                 bgfx.set_transform(as_void_ptr(mtx), 1)
 
                 # Set vertex and index buffer.
                 bgfx.set_vertex_buffer(0, self.vertex_buffer, 0, num_vertices)
-                bgfx.set_index_buffer(self.index_buffer,0,cube_indices.size)
+                bgfx.set_index_buffer(self.index_buffer, 0, cube_indices.size)
 
                 # Set the texture
                 bgfx.set_texture(0, self.texture_uniform, self.logo_texture)
 
                 bgfx.set_state(0
-                    | BGFX_STATE_WRITE_RGB 
-                    | BGFX_STATE_WRITE_A 
-                    | BGFX_STATE_WRITE_Z
-                    | BGFX_STATE_DEPTH_TEST_LESS
-                    | BGFX_STATE_MSAA,
-                    0,
-                )
+                               | BGFX_STATE_WRITE_RGB
+                               | BGFX_STATE_WRITE_A
+                               | BGFX_STATE_WRITE_Z
+                               | BGFX_STATE_DEPTH_TEST_LESS
+                               | BGFX_STATE_MSAA,
+                               0,
+                               )
 
                 bgfx.submit(0, self.main_program, 0, False)
 
@@ -238,6 +225,7 @@ class Textures(ExampleWindow):
         bgfx.reset(
             self.width, self.height, BGFX_RESET_VSYNC, self.init_conf.resolution.format
         )
+
 
 if __name__ == "__main__":
     textures = Textures(1280, 720, "examples/textures")
