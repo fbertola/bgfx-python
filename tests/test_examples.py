ab3717240d7f77d8a68f0041055a363c7805416d
Federico Bertola <8020907+fbertola@users.noreply.github.com>
2020-08-12 15:59:33 +0200

    Fixed tests cleanup

--- tests/test_examples.py
+++ tests/test_examples.py
@@ -14,18 +14,15 @@ def test_helloworld_example():
     HelloWorld.__bases__ = (OSMesaContext,)
     test = HelloWorld(1280, 720, "Test")
     test.run()
-    test.shutdown()
 
 
 def test_cubes_example():
     Cubes.__bases__ = (OSMesaContext,)
     test = Cubes(1280, 720, "Test")
     test.run()
-    test.shutdown()
 
 
 def test_mesh_example():
     MeshExample.__bases__ = (OSMesaContext,)
     test = MeshExample(1280, 720, "Test")
     test.run()
-    test.shutdown()
