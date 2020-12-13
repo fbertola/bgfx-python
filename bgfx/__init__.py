96f97beb931aeb2bf00240d5330ea853efb84521
Federico Bertola <8020907+fbertola@users.noreply.github.com>
2020-12-13 16:38:04 +0100

    First Cppyy test integration

--- bgfx/__init__.py
+++ bgfx/__init__.py
@@ -1,3 +1,11 @@
+import os
+import cppyy
+from .initializor import initialise
 from .constants import *
 from .utils import *
 from bgfx.bgfx_lib import *
+
+
+initialise('bgfx_python', 'libbgfx_pythonCppyy.dylib', 'bgfx_python.map')
+del initialise
+
---
+++ src/bgfx_python/__init__.py
@@ -0,0 +1,7 @@
+# import os
+# import cppyy
+# from .initializor import initialise
+# initialise('bgfx_python', 'libbgfx_pythonCppyy.dylib', 'bgfx_python.map')
+# del initialise
+
+
