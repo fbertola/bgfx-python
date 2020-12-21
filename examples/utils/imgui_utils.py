ba1a1a9e20055148a95f46c7e637bf0d30fb9660
Federico Bertola <8020907+fbertola@users.noreply.github.com>
2020-12-21 16:51:06 +0100

    HelloWorld example now works

--- examples/utils/imgui_utils.py
+++ examples/utils/imgui_utils.py
@@ -1,6 +1,7 @@
 from array import array
 
-from bgfx import bgfx, ImGui
+# noinspection PyUnresolvedReferences
+from pybgfx import bgfx, ImGui
 
 
 class SampleData:
@@ -39,193 +40,197 @@ class SampleData:
 
 
 def bar(width, max_width, height, color):
-    style = ImGui.get_style()
+    style = ImGui.GetStyle()
 
-    hovered_color = ImGui.Vec4(
+    hovered_color = ImGui.ImVec4(
         color.x + color.x * 0.1,
         color.y + color.y * 0.1,
         color.z + color.z * 0.1,
         color.w + color.w * 0.1,
     )
 
-    ImGui.push_style_color(ImGui.Colors.Button, color)
-    ImGui.push_style_color(ImGui.Colors.ButtonHovered, hovered_color)
-    ImGui.push_style_color(ImGui.Colors.ButtonActive, color)
-    ImGui.push_style_var(ImGui.Style.FrameRounding, 0.0)
-    ImGui.push_style_var(ImGui.Style.ItemSpacing, ImGui.Vec2(0.0, style.item_spacing.y))
+    ImGui.PushStyleColor(ImGui.ImGuiCol_Button, color)
+    ImGui.PushStyleColor(ImGui.ImGuiCol_ButtonHovered, hovered_color)
+    ImGui.PushStyleColor(ImGui.ImGuiCol_ButtonActive, color)
+    ImGui.PushStyleVar(ImGui.ImGuiStyleVar_FrameRounding, 0.0)
+    ImGui.PushStyleVar(
+        ImGui.ImGuiStyleVar_ItemSpacing, ImGui.ImVec2(0.0, style.ItemSpacing.y)
+    )
 
     item_hovered = False
 
-    ImGui.button("", ImGui.Vec2(width, height))
-    item_hovered |= ImGui.is_item_hovered(0)
+    ImGui.Button("", ImGui.ImVec2(width, height))
+    item_hovered |= ImGui.IsItemHovered(0)
 
-    ImGui.same_line()
-    ImGui.invisible_button("", ImGui.Vec2(max(1.0, max_width - width), height), 0)
-    item_hovered |= ImGui.is_item_hovered(0)
+    ImGui.SameLine()
+    ImGui.InvisibleButton("", ImGui.ImVec2(max(1.0, max_width - width), height), 0)
+    item_hovered |= ImGui.IsItemHovered(0)
 
-    ImGui.pop_style_var(2)
-    ImGui.pop_style_color(3)
+    ImGui.PopStyleVar(2)
+    ImGui.PopStyleColor(3)
 
     return item_hovered
 
 
-s_resourceColor = ImGui.Vec4(0.5, 0.5, 0.5, 1.0)
+s_resourceColor = ImGui.ImVec4(0.5, 0.5, 0.5, 1.0)
 s_frame_time = SampleData()
 
 
 def resource_bar(name, tooltip, num, _max, max_width, height):
     item_hovered = False
 
-    ImGui.text(f"{name}: {num:4d} / {_max:4d}")
-    item_hovered |= ImGui.is_item_hovered(0)
-    ImGui.same_line()
+    ImGui.Text(f"{name}: {num:4d} / {_max:4d}")
+    item_hovered |= ImGui.IsItemHovered(0)
+    ImGui.SameLine()
 
     percentage = float(num) / float(_max)
 
     item_hovered |= bar(
         max(1.0, percentage * max_width), max_width, height, s_resourceColor
     )
-    ImGui.same_line()
+    ImGui.SameLine()
 
-    ImGui.text(f"{(percentage * 100.0):5.2f}%")
+    ImGui.Text(f"{(percentage * 100.0):5.2f}%")
 
     if item_hovered:
-        ImGui.set_tooltip(f"{tooltip} {(percentage * 100.0):5.2f}%")
+        ImGui.SetTooltip(f"{tooltip} {(percentage * 100.0):5.2f}%")
 
 
 def show_example_dialog():
-    ImGui.set_next_window_pos(ImGui.Vec2(10.0, 70.0), ImGui.Condition.FirstUseEver)
-    ImGui.set_next_window_size(ImGui.Vec2(300.0, 500.0), ImGui.Condition.FirstUseEver)
+    ImGui.SetNextWindowPos(ImGui.ImVec2(10.0, 70.0), ImGui.ImGuiCond_FirstUseEver)
+    ImGui.SetNextWindowSize(ImGui.ImVec2(325.0, 500.0), ImGui.ImGuiCond_FirstUseEver)
 
-    ImGui.begin("\uf080 Statistics")
-    renderer_name = bgfx.get_renderer_name(bgfx.get_renderer_type())
-    ImGui.text_wrapped(f"Current renderer: {renderer_name}")
-    ImGui.separator()
+    ImGui.Begin("\uf080 Statistics")
+    renderer_name = bgfx.getRendererName(bgfx.getRendererType())
+    ImGui.TextWrapped("Current renderer: {}".format(renderer_name))
+    ImGui.Separator()
 
-    stats = bgfx.get_stats()
-    to_ms_cpu = 1000.0 / stats.cpu_timer_freq
-    to_ms_gpu = 1000.0 / stats.gpu_timer_freq
-    frame_ms = max(float(stats.cpu_time_end - stats.cpu_time_begin), 1.0e-9)
+    stats = bgfx.getStats()
+    to_ms_cpu = 1000.0 / stats.cpuTimerFreq
+    to_ms_gpu = 1000.0 / stats.gpuTimerFreq
+    frame_ms = max(float(stats.cpuTimeEnd - stats.cpuTimeBegin), 1.0e-9)
 
     s_frame_time.push_sample(frame_ms * to_ms_cpu)
 
-    frame_text_overlay = f"\uf063{s_frame_time.m_min:7.3f}ms, \uf062{s_frame_time.m_max:7.3f}ms\nAvg: {s_frame_time.m_avg:7.3f}ms, {(stats.cpu_timer_freq / frame_ms):6.2f} FPS"
-    ImGui.push_style_color(ImGui.Colors.PlotHistogram, ImGui.Vec4(0.0, 0.5, 0.15, 1.0))
-    ImGui.push_item_width(-1)
-    ImGui.plot_histogram(
+    frame_text_overlay = f"\uf063{s_frame_time.m_min:7.3f}ms, \uf062{s_frame_time.m_max:7.3f}ms\nAvg: {s_frame_time.m_avg:7.3f}ms, {(stats.cpuTimerFreq / frame_ms):6.2f} FPS"
+    ImGui.PushStyleColor(
+        ImGui.ImGuiCol_PlotHistogram, ImGui.ImVec4(0.0, 0.5, 0.15, 1.0)
+    )
+    ImGui.PushItemWidth(-1)
+    ImGui.PlotHistogram(
         "",
-        array("f", s_frame_time.m_values)[0],
+        array("f", s_frame_time.m_values),
         100,
         s_frame_time.m_offset,
         frame_text_overlay,
         0.0,
         60.0,
-        ImGui.Vec2(0.0, 45.0),
+        ImGui.ImVec2(0.0, 45.0),
     )
-    ImGui.pop_item_width()
-    ImGui.pop_style_color()
+    ImGui.PopItemWidth()
+    ImGui.PopStyleColor()
 
-    ImGui.text(
-        f"Submit CPU {(stats.cpu_time_end - stats.cpu_time_begin) * to_ms_cpu:.3f}, GPU {(stats.gpu_time_end - stats.gpu_time_begin) * to_ms_gpu:.3f} (L: {stats.max_gpu_latency})"
+    ImGui.Text(
+        f"Submit CPU {(stats.cpuTimeEnd - stats.cpuTimeBegin) * to_ms_cpu:.3f}, GPU {(stats.gpuTimeEnd - stats.gpuTimeBegin) * to_ms_gpu:.3f} (L: {stats.maxGpuLatency})"
     )
 
-    if stats.gpu_memory_max > 0:
-        ImGui.text(f"GPU mem: {stats.gpu_memory_used} / {stats.gpu_memory_max}")
+    if stats.gpuMemoryMax > 0:
+        ImGui.Text(f"GPU mem: {stats.gpuMemoryUsed} / {stats.gpuMemoryMax}")
 
-    if ImGui.collapsing_header("\uf12e Resources", 1 << 5):
-        caps = bgfx.get_caps()
-        item_height = ImGui.get_text_line_height_with_spacing()
-        max_width = 90.0
-        ImGui.push_font(ImGui.Font.Mono)
-        ImGui.text("Res: Num  / Max")
+    if ImGui.CollapsingHeader("\uf12e Resources", ImGui.ImGuiTreeNodeFlags_DefaultOpen):
+        caps = bgfx.getCaps()
+        item_height = ImGui.GetTextLineHeightWithSpacing()
+        max_width = 115.0
+        ImGui.PushFont(ImGui.Font.Mono)
+        ImGui.Text("Res: Num  / Max")
         resource_bar(
             "DIB",
             "Dynamic index buffers",
-            stats.num_dynamic_index_buffers,
-            caps.limits.max_dynamic_index_buffers,
+            stats.numDynamicIndexBuffers,
+            caps.limits.maxDynamicIndexBuffers,
             max_width,
             item_height,
         )
         resource_bar(
             "DVB",
             "Dynamic vertex buffers",
-            stats.num_dynamic_vertex_buffers,
-            caps.limits.max_dynamic_vertex_buffers,
+            stats.numDynamicVertexBuffers,
+            caps.limits.maxDynamicVertexBuffers,
             max_width,
             item_height,
         )
         resource_bar(
             " FB",
             "Frame buffers",
-            stats.num_frame_buffers,
-            caps.limits.max_frame_buffers,
+            stats.numFrameBuffers,
+            caps.limits.maxFrameBuffers,
             max_width,
             item_height,
         )
         resource_bar(
             " IB",
             "Index buffers",
-            stats.num_index_buffers,
-            caps.limits.max_index_buffers,
+            stats.numIndexBuffers,
+            caps.limits.maxIndexBuffers,
             max_width,
             item_height,
         )
         resource_bar(
             " OQ",
             "Occlusion queries",
-            stats.num_occlusion_queries,
-            caps.limits.max_occlusion_queries,
+            stats.numOcclusionQueries,
+            caps.limits.maxOcclusionQueries,
             max_width,
             item_height,
         )
         resource_bar(
             "  P",
             "Programs",
-            stats.num_programs,
-            caps.limits.max_programs,
+            stats.numPrograms,
+            caps.limits.maxPrograms,
             max_width,
             item_height,
         )
         resource_bar(
             "  S",
             "Shaders",
-            stats.num_shaders,
-            caps.limits.max_shaders,
+            stats.numShaders,
+            caps.limits.maxShaders,
             max_width,
             item_height,
         )
         resource_bar(
             "  T",
             "Textures",
-            stats.num_textures,
-            caps.limits.max_textures,
+            stats.numTextures,
+            caps.limits.maxTextures,
             max_width,
             item_height,
         )
         resource_bar(
             "  U",
             "Uniforms",
-            stats.num_uniforms,
-            caps.limits.max_uniforms,
+            stats.numUniforms,
+            caps.limits.maxUniforms,
             max_width,
             item_height,
         )
         resource_bar(
             " VB",
             "Vertex buffers",
-            stats.num_vertex_buffers,
-            caps.limits.max_vertex_buffers,
+            stats.numVertexBuffers,
+            caps.limits.maxVertexBuffers,
             max_width,
             item_height,
         )
         resource_bar(
             " VL",
             "Vertex layouts",
-            stats.num_vertex_layouts,
-            caps.limits.max_vertex_layouts,
+            stats.numVertexLayouts,
+            caps.limits.maxVertexLayouts,
             max_width,
             item_height,
         )
-        ImGui.pop_font()
+        ImGui.PopFont()
 
-    ImGui.end()
+    ImGui.End()
