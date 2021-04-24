204b19ad6d7da787ffefddaba2d9299236d001b3
Federico Bertola <federico.bertola88@gmail.com>
2021-04-25 00:25:00 +0200

    Fixed Windows dll name

--- pybgfx/__init__.py
+++ pybgfx/__init__.py
@@ -8,7 +8,7 @@ if sys_platform == "Darwin":
 elif sys_platform == "Linux":
     _lib_name = "libbgfx_pythonCppyy.so"
 else:
-    _lib_name = "libbgfx_pythonCppyy.dll"
+    _lib_name = "bgfx_pythonCppyy.dll"
 
 initialise("pybgfx", _lib_name, "bgfx_python.map")
 del initialise
