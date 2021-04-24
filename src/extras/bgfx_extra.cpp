639f340c726bc8b040396103f2c5775f7743588a
Federico Bertola <federico.bertola88@gmail.com>
2021-04-25 00:24:35 +0200

    Updated dependencies

---
+++ src/extras/bgfx_extra.cpp
@@ -0,0 +1,31 @@
+#include "bgfx_extra.h"
+
+#include <bx/file.h>
+
+bx::DefaultAllocator default_allocator;
+bx::AllocatorI * allocator = & default_allocator;
+
+Mesh* meshLoad(const char* _filePath, bool _ramcopy)
+{
+    bx::FileReaderI* reader = BX_NEW(allocator, bx::FileReader);
+    if (bx::open(reader, _filePath) )
+    {
+        Mesh* mesh = new Mesh;
+        mesh->load(reader, _ramcopy);
+        bx::close(reader);
+        return mesh;
+    }
+    BX_DELETE(allocator, reader);
+    return NULL;
+}
+
+Mesh* _MeshLoader::load(const char* _filePath, bool _ramcopy)
+{
+    return meshLoad(_filePath, _ramcopy);
+}
+
+void _MeshLoader::destroy(Mesh* _mesh)
+{
+    _mesh->unload();
+    delete _mesh;
+}
