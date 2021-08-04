e6192e882d36a593f9300c90ad5634f594fbc2f8
Federico Bertola <8020907+fbertola@users.noreply.github.com>
2021-08-04 11:21:03 +0200

    Fixed mesh example

--- src/extras/bgfx_extra.cpp
+++ src/extras/bgfx_extra.cpp
@@ -2,13 +2,12 @@
 
 #include <bx/file.h>
 
-#if defined(WIN32)
 
 bx::DefaultAllocator default_allocator;
 bx::AllocatorI * allocator = & default_allocator;
 
 
-Mesh* meshLoad(const char* _filePath, bool _ramcopy)
+Mesh* meshLoadInternal(const char* _filePath, bool _ramcopy)
 {
     bx::FileReaderI* reader = BX_NEW(allocator, bx::FileReader);
     if (bx::open(reader, _filePath) )
@@ -22,15 +21,9 @@ Mesh* meshLoad(const char* _filePath, bool _ramcopy)
     return NULL;
 }
 
-#else
-
-Mesh* meshLoad(const char* _filePath, bool _ramcopy);
-
-#endif
-
 Mesh* _MeshLoader::load(const char* _filePath, bool _ramcopy)
 {
-    return meshLoad(_filePath, _ramcopy);
+    return meshLoadInternal(_filePath, _ramcopy);
 }
 
 void _MeshLoader::destroy(Mesh* _mesh)
